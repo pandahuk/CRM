6da336c139550f90b576f549e1e5a8cac9ece216
Desmond Lee <d1031lee@Gmail.com>
2019-03-08 14:53:13 -0800

    Adding in basic methods

--- src/Model/CRM.java
+++ src/Model/CRM.java
@@ -3,6 +3,39 @@ package Model;
 
 public class CRM {
     
-    //create all needed variables and functions to have a CRM.
-    private int num = 5;
+    private String companyName;
+    //private ArrayList<Accounts> account;
+    //private ArrayList<Accounts> opportunity;
+    //private ArrayList<Leads> leads;
+    
+    public void CRM(String name) {
+        //initialize
+        this.companyName = name;
+        //this.account = new 
+        //this.leads = new 
+    }
+    
+    public String getName(){
+        return this.companyName;
+    }
+    
+    public void addContact(){
+        
+    }
+    
+    public void updateContact(){
+        
+    }
+    
+    @Override
+    public String toString(){
+        return "CRM";
+        
+        //list of accounts
+            //list
+        //list of opp
+            //opp
+        //list of leads
+            //leads
+    }
 }
